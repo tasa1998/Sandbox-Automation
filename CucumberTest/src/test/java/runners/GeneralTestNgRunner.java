a66692b56359a85cc7b6e8ccbbb3150a7a2d0523
kgigic <katarina.gigic13@gmail.com>
2023-11-02 11:36:36 -0400

    add runners

---
+++ CucumberTest/src/test/java/runners/GeneralTestNgRunner.java
@@ -0,0 +1,22 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+@Test
+@CucumberOptions(
+        features = "src/test/java/features/SandboxPractice.feature",
+        glue= "steps",
+        tags="@Homeowner"
+)
+public class GeneralTestNgRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios(){
+        return super.scenarios();
+    }
+}
